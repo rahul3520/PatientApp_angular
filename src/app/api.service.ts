b3092e71dda7cd486aa3f900ecc998d47e3714b5
rahulg3520 <rahul@nestgroup.net>
2022-12-07 03:33:10 +0530

    view patient page

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -12,4 +12,9 @@ export class ApiService {
   {
       return this.http.post("http://localhost:8080/add",dataToSend)
   }
+
+  viewPatientData=()=>
+  {
+    return this.http.get("http://localhost:8080/view")
+  }
 }
